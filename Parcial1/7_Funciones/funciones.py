bfcd660aa62930d25ffc35b15e9e60b5ac28118e
0DECMA0 <miguelangeldelgadocabrera15@gmail.com>
2024-05-31 09:06:06 -0600

    Nuevo

---
+++ Parcial1/7_Funciones/funciones.py
@@ -0,0 +1,33 @@
+""" Una funcion es un conjunto de instrucciones agrupadas bajo un nombre en particular como un prohtama mas pequeño que cumple una funcion especifica la funcion se puede reutilizar con el simple hecho de invocarla es decir mandarla llamar
+
+intaxis:
+ def nombredeMifuncion(parametros):
+ bloque o conjunto de instrucciones
+
+nombredeMifuncion(parametros=)
+
+Las funciones pueden ser de 4 tipos 
+1,- Funcion que no recibe parametros y no regresa Valor
+2,- Funcion que no recibe parametros y regresa Valor
+3,- Funcion que recibe parametros y no regresa Valor
+4,- Funcion que  recibe parametros y regresa Valor
+
+Temas a ver: funciones, lista, set, duplas y diccionarios
+"""
+
+#Crear una funcion para imprimir nombre de personas 
+# 1,- Funcion que no recibe parametros y no regresa Valor
+def solicitarNombre():
+    nombre=input("Ingresa el nombre comleto:")
+
+solicitarNombre()
+
+def suma():
+    n1=int(input("Numero #1: "))
+    n2=int(input("Numero #2: "))
+    suma=n1+n2
+    print(f"{n1}+{n2}={suma}")
+
+suma()
+#Crea
+
