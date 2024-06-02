46db123a5cdbb617710fbce2708a3dbc332faafe
0DECMA0 <miguelangeldelgadocabrera15@gmail.com>
2024-06-02 11:18:19 -0600

    a

---
+++ Parcial1/7_Funciones/calculadora_basica.py
@@ -0,0 +1,49 @@
+
+"""opcion=True
+while opcion:
+    print("\n \t Calculadora basica, \n Escoge una opcion \n 1-Suma \n 2-Resta \n 3-Multiplicacion \n 4-Divicion \n 5-Salir")
+    opcion=input("\t Elige una opcion").upper()
+    
+    if opcion=="1" or opcion=="+" or opcion=="SUMA":
+        n1=int(input("Numero #1:"))
+        n2=int(input("Numero #2:"))
+        print(f"{n1}+{n2}={n1+n2}")
+    elif opcion=="2" or opcion=="-" or opcion=="RESTA":
+        n1=int(input("Numero #1:"))
+        n2=int(input("Numero #2:"))
+        print(f"{n1}-{n2}={n1-n2}")
+    elif opcion=="3" or opcion=="*" or opcion=="MULTIPLICACION":
+        n1=int(input("Numero #1:"))
+        n2=int(input("Numero #2:"))
+        print(f"{n1}*{n2}={n1*n2}")
+    elif opcion=="4" or opcion=="/" or opcion=="DIVICION":
+        n1=int(input("Numero #1:"))
+        n2=int(input("Numero #2:"))
+        print(f"{n1}/{n2}={n1/n2}")
+    else
+        print("Gracias , Terminaste la ejecucion del programa") """
+
+def solicitarNumero():
+    global n1,n2
+    n1=int(input("Numero #1:"))
+    n2=int(input("Numero #2:"))
+    return n1,n2
+
+def operacionAritmetica(num1,num2):
+    if opcion=="1" or opcion=="+" or opcion=="SUMA":
+        return f"{n1}+{n2}={n1+n2}"
+    elif opcion=="2" or opcion=="-" or opcion=="RESTA":
+        return f"{n1}-{n2}={n1-n2}"
+    elif opcion=="3" or opcion=="*" or opcion=="MULTIPLICACION":
+        return f"{n1}*{n2}={n1*n2}"
+    elif opcion=="4" or opcion=="/" or opcion=="DIVICION":
+        return f"{n1}/{n2}={n1/n2}"
+    else:
+        opcion=False
+        return "Gracias , Terminaste la ejecucion del programa"
+opcion1=True
+while opcion1:
+    print("\n \t Calculadora basica, \n Escoge una opcion \n 1-Suma \n 2-Resta \n 3-Multiplicacion \n 4-Divicion \n 5-Salir")
+    opcion1=input("\t Elige una opcion").upper()
+    solicitarNumero()
+    print(operacionAritmetica(n1,n2))
